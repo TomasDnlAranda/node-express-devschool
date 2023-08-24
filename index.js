a789f09a82ff3888c7a8725e7e824fbb1972fab4
Tomas Aranda <tomasdeveloperfrontend@gmail.com>
2023-08-24 15:36:48 -0300

    comments

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const port = 3977;
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}/`);
+		await mongoose.connect(
+			`mongodb+srv://tomasdeveloperfrontend:admin123456@cluster0.a6t72jk.mongodb.net/`
+		);
 		app.listen(port, () => {
 			console.log('====================================');
 			console.log('============= API REST =============');
 			console.log('====================================');
-			console.log(`http://${ipServer}:${port}/api/${apiVersion}`);
+			console.log(`http://localhost:${port}/api/v1`);
 		});
 	} catch (err) {
 		console.log('Error al conectar a la base de datos', err);
